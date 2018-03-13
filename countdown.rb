b584fa614df90a895216186792ea04c110e45547
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-03-13 21:08:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
-end
+def countdown(interger)
+  counter = interger 
+  while interger > 0 
+  puts "#{interger} SECOND(S)!"
+  interger += 1
+end 
+  return "HAPPY NEW YEAR!"
+end 
+
